5130f7d6c924a124817f9f7cabde73fd5c985eed
TamilarasanG17 <tamilarasang17@gmail.com>
2026-04-21 23:18:53 +0530

    driver entity setup

---
+++ src/main/java/com/example/fleetmanagement/model/Driver.java
@@ -0,0 +1,20 @@
+package com.example.fleetmanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String licenseNumber;
+    private String phone;
+}
