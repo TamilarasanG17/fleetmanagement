fd5934d99fd7eea5ab347d7474e40d7fcd32fcb7
TamilarasanG17 <tamilarasang17@gmail.com>
2026-04-21 23:12:01 +0530

    first commit

---
+++ src/test/java/com/example/feetmanagement/FeetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeetmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
