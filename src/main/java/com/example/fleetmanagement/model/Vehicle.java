e6fd1a910a998bb8ca371bd2ae64497709b098fd
TamilarasanG17 <tamilarasang17@gmail.com>
2026-04-22 12:51:36 +0530

    Vechicle entity created:

--- src/main/java/com/example/fleetmanagement/model/Vehicle.java
+++ src/main/java/com/example/fleetmanagement/model/Vehicle.java
@@ -4,22 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-// import lombok.Data;
 
 @Entity
-@Table(name = "drivers")
-public class Driver {
+@Table(name = "vehicles")
+public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String licenseNumber;
-    private Integer shiftHours;
+    private String licensePlate;
+    private Double capacity;
+    private String maintenanceStatus;
 
-    @OneToOne
-    @JoinColumn(name = "vehicle_id")
-    private Vehicle vehicle;
+    @OneToOne(mappedBy = "vehicle")
+    private Driver driver;
 }
