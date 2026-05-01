c132bbf24d2f491e2fe55b7a2c5a6de898ec9c72
TamilarasanG17 <tamilarasang17@gmail.com>
2026-05-01 14:09:55 +0530

    swagger depency added

---
+++ src/main/java/com/example/fleetmanagement/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.example.fleetmanagement.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Fleet Management API")
+                        .version("1.0")
+                        .description("API for Drivers, Vehicles, Routes, Tasks and Optimization"));
+    }
+}
