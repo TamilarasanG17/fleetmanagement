ab4b423e32e63638e11fa26ce6a28fb03a4a5b0d
TamilarasanG17 <tamilarasang17@gmail.com>
2026-05-03 13:38:02 +0530

    update main file

--- src/main/java/com/example/fleetmanagement/FeetmanagementApplication.java
+++ src/main/java/com/example/fleetmanagement/FeetmanagementApplication.java
@@ -3,10 +3,25 @@ package com.example.fleetmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class FeetmanagementApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().directory("./").ignoreIfMalformed().ignoreIfMissing().load();
+
+		if(dotenv.get("DB_URL") != null) {
+			System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		}
+		if(dotenv.get("DB_USER") != null) {
+			System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		}
+		if(dotenv.get("DB_PASSWORD") != null) {
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		}
+
 		SpringApplication.run(FeetmanagementApplication.class, args);
 	}
 
